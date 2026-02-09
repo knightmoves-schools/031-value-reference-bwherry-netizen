727a156d075b49f70305bbd302979260e9c02d61
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-09 12:29:16 -0600

    Implement ledger update and tax rate adjustment methods
    Added methods to update ledger and adjust tax rate.

--- Account.cs
+++ Account.cs
@@ -3,7 +3,19 @@
 public class Account {
     public static int OriginalTaxRate = 10;
     public static int[] CurrentLedger = {10, 20, 30, 40, 50};
-
-    // Add your code here
 }
+
+public class NewTaxRate = OriginalTaxRate;
+public class CopyLedger = CurrentLedger;
+
+public int UpdateLedger (int correction) 
+{
+    CurrentLedger[0] = 15;
+}
+
+public int AdjustTaxRate (int taxRate)
+{
+    
+}
+
 
