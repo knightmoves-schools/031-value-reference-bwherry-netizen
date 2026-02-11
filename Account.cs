462edb1266d2a1df12b7c7b288ac2a991cd8bfdb
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-11 10:43:00 -0600

    Refactor Account class methods and structure

--- Account.cs
+++ Account.cs
@@ -7,7 +7,7 @@ public class Account {
     public int NewTaxRate = OriginalTaxRate;
     public int[] CopyLedger = CurrentLedger;
 
-public int UpdateLedger (int correction) 
+public int[] UpdateLedger (int correction) 
 {
     correction = 15;
     CurrentLedger[0] = correction;
@@ -21,5 +21,6 @@ public int AdjustTaxRate (int taxRate)
     return NewTaxRate;
 }
 }
+
 
 
