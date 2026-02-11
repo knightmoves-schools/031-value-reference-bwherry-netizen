f3aef1dfdb32a98f934b779a8f865a1281ad59d3
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-11 10:39:38 -0600

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -7,7 +7,7 @@ public class Account {
     public int NewTaxRate = OriginalTaxRate;
     public int[] CopyLedger = CurrentLedger;
 
-public int[] UpdateLedger (int correction) 
+public int UpdateLedger (int correction) 
 {
     correction = 15
     CurrentLedger[0] = correction;
@@ -21,3 +21,4 @@ public int AdjustTaxRate (int taxRate)
     return NewTaxRate;
 }
 }
+
